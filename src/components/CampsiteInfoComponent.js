04f56ec086dbfab5408e0b6ca4fea3ca017a3238
Sheila Strahan <smstrahan@gmail.com>
2021-12-01 20:43:42 -0700

    fetch post comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -53,7 +53,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text)
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text)
     }
 
     render() { 
@@ -110,7 +110,7 @@ class CommentForm extends Component {
     }
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -128,7 +128,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                     );
                 })}
                 </Stagger>
-                <CommentForm campsiteId={campsiteId} addComment={addComment}/>
+                <CommentForm campsiteId={campsiteId} postComment={postComment}/>
             </div>
         );
     }
@@ -173,7 +173,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                 </div>
